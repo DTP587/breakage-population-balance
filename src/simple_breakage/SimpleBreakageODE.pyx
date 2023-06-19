2b389cb8012834b7002432a585f1abb24a7b70e0
RussianMillitaryBlimp <diego.tomohisa@gmail.com>
2023-06-19 11:14:02 +0100

    Initial Commit
    Just working out how package structures work.

---
+++ src/simple_breakage/SimpleBreakageODE.pyx
@@ -0,0 +1,37 @@
+import cython
+import numpy as np
+cimport numpy as np
+
+np.import_array()
+
+DTYPE_INT = np.int64
+DTYPE_FLOAT = np.float64
+
+ctypedef np.int64_t DTYPE_INT_t
+ctypedef np.float64_t DTYPE_FLOAT_t
+
+# For use with scipy's ODEINT
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def breakage_ode(
+    np.ndarray[DTYPE_FLOAT_t, ndim=1] N,
+    DTYPE_FLOAT_t t,
+    np.ndarray[DTYPE_FLOAT_t, ndim=1] x,
+    np.ndarray[DTYPE_FLOAT_t, ndim=1] rate,
+    np.ndarray[DTYPE_FLOAT_t, ndim=2] kernel
+):
+    
+    cdef int bins = N.shape[0]
+
+    cdef int i
+    cdef int j
+
+    cdef np.ndarray[DTYPE_FLOAT_t, ndim=1] n = np.zeros(bins)
+
+    n -= rate * N
+
+    for i in np.arange(bins):
+        for j in np.arange(i+1, bins):
+            n[i] += rate[j] * kernel[i, j] * N[j]
+
+    return n
