26eb9b0c0b3388e9dc8c122412350e17a8abffc7
RussianMillitaryBlimp <diego.tomohisa@gmail.com>
2023-06-20 12:35:32 +0100

    Working module

---
+++ README.md
@@ -0,0 +1,13 @@
+# breakage-population-balance
+
+_Solution to breakage-based population balance equations in python._
+
+Would look to increase the number of equations supported. At the moment this module only supports simple, 1D cases (pure breakage). Currently only solves for fraction, which simplifies calculations for mass/volume based systems.
+
+<img src="./images/test.png"  width="600">
+
+_Comparison between analytical [(Ziff and McGrady, 1985)](https://www.doi.org/10.1088/0305-4470/18/15/026
+) and numerical methods._
+
+
+
--- breakage_population_balance/__init__.py
+++ breakage_population_balance/__init__.py
@@ -5,53 +5,61 @@ import importlib
 module_name = "breakageODE"
 suffix = ".so"
 
-module_files = glob(os.path.join(os.path.dirname(__file__), f"{module_name}.*{suffix}"))
+module_file = glob(
+    os.path.join(
+        os.path.dirname(__file__), f"{module_name}.*{suffix}"
+    )
+)[0]
 
-if module_files:
-    # Use the first matching file
-    module_file = module_files[0]
-
+if module_file:
     # Import the module
-    module_spec = importlib.util.spec_from_file_location(module_name, module_file)
-    module = importlib.util.module_from_spec(module_spec)
-    module_spec.loader.exec_module(module)
+    module_spec = importlib.util.spec_from_file_location(
+        module_name, module_file
+    )
+    breakageODE = importlib.util.module_from_spec(module_spec)
+    module_spec.loader.exec_module(breakageODE)
 else:
     raise ValueError(f"Module {module_name} not found.")
 
 import numpy as np
 from scipy.integrate import odeint
 
-# =============================================================================================
-
-# Currently only supports 1 Dimension in internal geometry.
-class breakageModel():
-    def __init__(self, initial_condition, times, grid, kernel, rate):
-        self._IC  = __set_ICs(initial_condition)
-        self._t   = __set_times(times)
-        self._x   = __set_grid(grid)
-        self._Phi = __set_kernel(kernel)
-        self._k   = __set_rate(rate)
-    
-    def solve(self):
-        # Solve the problem
-        return odeint(simple_breakage, self._IC, self._t, args=(self._x, self._k, self._Phi))
-    
-# --------------------------------------------
+# =============================================================================
 
 def check_ndarray(*args, **kwargs):
-    if not isinstance(args[0], np.ndarray):
-        raise ValueError(f"Argument '{func.__code__.co_varnames[0]}' is not of type numpy.ndarray.")
+    if not isinstance(args[1], np.ndarray):
+        raise ValueError(f"Argument '{args[1]}' is not of type numpy.ndarray.")
 
 def wrapper_check_ndarray(func):
     def wrapper(*args, **kwargs):
         check_ndarray(*args, **kwargs)
         return func(*args, **kwargs)
     return wrapper
+
+# =============================================================================
+
+# Currently only supports 1 Dimension in internal geometry.
+class breakageModel():
+    def __init__(self, initial_condition, times, grid, kernel, rate):
+        self._IC  = self.__set_ICs(initial_condition)
+        self._t   = self.__set_times(times)
+        self._x   = self.__set_grid(grid)
+        self._Phi = self.__set_kernel(kernel)
+        self._k   = self.__set_rate(rate)
     
+    def solve(self):
+        # Solve the problem
+        return odeint(
+            breakageODE.simple_breakage, self._IC, self._t,
+            args=(
+                self._x, self._k, self._Phi
+            )
+        )
+
 # --------------------------------------------
 
     @wrapper_check_ndarray 
-    def __set_ICs(initial_condition):
+    def __set_ICs(self, initial_condition):
         # Cannot support > 1D
         assert len(initial_condition.shape) < 2
         # Set the number of classes (bins for brevity)
@@ -59,17 +67,17 @@ def wrapper_check_ndarray(func):
         return initial_condition
 
     @wrapper_check_ndarray    
-    def __set_times(times):        
+    def __set_times(self, times):        
         return times
 
     @wrapper_check_ndarray 
-    def __set_grid(grid):
+    def __set_grid(self, grid):
         # Cannot support > 1D
         assert len(grid.shape) <2
-        assert (grid.shape == self._bins)
+        assert (grid.shape[0] == self._bins)
         return grid
      
-    def __set_kernel(kernel):
+    def __set_kernel(self, kernel):
         # Cannot support > 1D
         if callable(kernel):
             # Calculate phi
@@ -88,7 +96,7 @@ def wrapper_check_ndarray(func):
         check_ndarray(kernel)
         return kernel
 
-    def __set_rate(rate):
+    def __set_rate(self, rate):
         #Cannot support > 1D
         if callable(rate):
             return rate(self._x)
